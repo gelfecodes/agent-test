a59e097645225ea99d1b0abd779f84daad511cf7
geekyfocus <gidabdulharkiem27@gmail.com>
2024-04-07 01:49:04 +0100

    2024-04-07T01:49:04

--- index.js
+++ index.js
@@ -22,6 +22,35 @@ const markCommit = (x, y) => {
   });
 };
 
+// const makeCommits = (n) => {
+//   if (n === 0) return simpleGit().push();
+//   const x = random.int(0, 54);
+//   const y = random.int(0, 6);
+//   const date = moment()
+//     .subtract(1, "y")
+//     .add(1, "d")
+//     .add(x, "w")
+//     .add(y, "d")
+//     .format();
+
+//   const data = {
+//     date: date,
+//   };
+//   console.log(date);
+//   jsonfile.writeFile(path, data, () => {
+//     simpleGit()
+//       .add([path])
+//       .commit(
+//         date,
+//         {
+//           "--date": date,
+//         },
+//         () => makeCommits(--n)
+//       );
+//     // .commit(date, { "--date": date }, makeCommits.bind(this, --n));
+//   });
+// };
+
 const makeCommits = (n) => {
   if (n === 0) return simpleGit().push();
   const x = random.int(0, 54);
@@ -31,16 +60,20 @@ const makeCommits = (n) => {
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
-    .format();
+    .format("YYYY-MM-DDTHH:mm:ss");
+  const data = { date: date };
 
-  const data = {
-    date: date,
-  };
-  console.log(date);
-  jsonfile.writeFile(path, data, () => {
+  console.log("Writing data:", data);
+  console.log("Making commit for date:", date);
+
+  jsonfile.writeFile(path, data, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
     simpleGit()
       .add([path])
-      .commit(date, { "--date": date }, makeCommits.bind(this, --n));
+      .commit(date, { "--date": date }, () => makeCommits(--n));
   });
 };
 
